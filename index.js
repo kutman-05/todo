3682f26dadc0140d5856031ab6bef4fc1767edcc
Kutman_kushbaokov <147912645+kutman-05@users.noreply.github.com>
2024-05-26 19:09:49 +0600

    todo

---
+++ index.js
@@ -0,0 +1,40 @@
+const addBtn = document.querySelector(".btn");
+const textInput = document.querySelector(".text");
+const numberInput = document.querySelector(".number");
+const newList = document.querySelector(".newList");
+const imageInput = document.querySelector(".image");
+
+addBtn.addEventListener("click", () => {
+  addNewList();
+});
+
+numberInput.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    addNewList();
+  }
+});
+
+function addNewList() {
+  if (textInput.value !== "" && numberInput.value !== "" && imageInput !== "") {
+    const h4 = document.createElement("h4");
+    h4.style.display = "flex";
+    h4.style.marginTop = "-5px";
+    h4.style.marginLeft = "40px";
+    h4.innerHTML = `<h3 class="kutman">${textInput.value}</h3>
+       <h2> ${numberInput.value}</h2>
+    <img src=${imageInput.value} alt="">/
+        `; //</br>
+
+    newList.append(h4);
+
+    textInput.value = "";
+    numberInput.value = "";
+    textInput.style.border = "";
+    numberInput.style.border = "";
+    imageInput.value = "";
+  } else if (textInput.value === "" || numberInput.value === "") {
+    textInput.style.border = "2px solid red";
+    numberInput.style.border = "2px solid red";
+    alert("заполните все поле!!");
+  }
+}
